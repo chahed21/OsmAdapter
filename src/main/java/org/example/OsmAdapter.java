13cf484810a1675f58f32cf4b09eb1d820c7b3d4
chahed21 <88270542+chahed21@users.noreply.github.com>
2023-10-20 08:25:27 +0200

    Adding map loader
    * 'Adding database dependencies'
    
    * 'Modifying database credentials'
    
    * 'Adding jooq dependencies'
    
    * 'Adding Map loader'

--- src/main/java/org/example/OsmAdapter.java
+++ src/main/java/org/example/OsmAdapter.java
@@ -1,8 +1,31 @@
 package org.example;
 
+import Loader.RoutableOSMMapLoader;
+import OpenLRImpl.MapDatabaseImpl;
+
+import java.sql.SQLException;
+
 public class OsmAdapter {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws SQLException {
         System.out.println("OsmAdapter");
+        RoutableOSMMapLoader OsmLoader = new RoutableOSMMapLoader();
+        /*System.out.println(OsmLoader.numberOfLines());
+        //System.out.println(osm.getAllLinesList());
+        System.out.println(OsmLoader.numberOfNodes());*/
+        MapDatabaseImpl MapDb = new MapDatabaseImpl(OsmLoader);
+        System.out.println(MapDb.getNumberOfLines());
+        System.out.println(MapDb.getNumberOfNodes());
+
+        //System.out.println(osm.getAllNodesList());
+        /*try {
+            GenerationTool.generate(
+                    Files.readString(Path.of("jooq-config.xml"))
+            );
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }*/
+
 
     }
 }
